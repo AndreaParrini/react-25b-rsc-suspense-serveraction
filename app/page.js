2c88dc06134253153b5cff9f849c86080c683a12
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-04-16 10:24:24 +0200

    add server actions

--- app/page.js
+++ app/page.js
@@ -1,11 +1,21 @@
 import ClientDemo from "@/components/ClientDemo";
 import DataFetchingDemo from "@/components/DataFetchingDemo";
 import RSCDemo from '@/components/RCSDemo';
+import ServerActionsDemo from "@/components/ServerActionsDemo";
 
 export default function Home() {
   return (
     <main>
-      <DataFetchingDemo />
+      {/* 
+      I componenti server possono essere utilizzati all'interno di componenti client SOLO se vengono passati come children come di seguito
+      Se importanti all'interno del componente client, di default vengono trasformati in client anche loro.
+      <ClientDemo>
+        <RSCDemo />
+      </ClientDemo> 
+      */}
+    
+      {/* <DataFetchingDemo /> */}
+      <ServerActionsDemo />
     </main>
   );
 }
