d7f20b708495323ca00feb40fc5607361a12ace9
Andrea.Parrini.Lavoro <andrea.parrini26@gmail.com>
2025-04-16 11:13:14 +0200

    add erro boundary

--- app/page.js
+++ app/page.js
@@ -6,12 +6,14 @@ import fs from 'node:fs/promises';
 
 import UsePromiseDemo from "@/components/UsePromisesDemo";
 import { Suspense } from "react";
+import ErrorBoundary from '@/components/ErrorBoundary';
 
 export default async function Home() {
-  const fetchUserPromise = new Promise((resolve) => setTimeout(async () => {
+  const fetchUserPromise = new Promise((resolve, reject) => setTimeout(async () => {
     const data = await fs.readFile('dummy-db.json', 'utf-8');
     const users = JSON.parse(data);
-    resolve(users);
+    //resolve(users);
+    reject(new Error('Error!'));
   }, 2000))
   
 
@@ -27,9 +29,12 @@ export default async function Home() {
     
       {/* <DataFetchingDemo /> */}
       {/* <ServerActionsDemo /> */}
-      <Suspense fallback={<p>Loading users...</p>}>
-        <UsePromiseDemo usersPromise={fetchUserPromise}/>
-      </Suspense> 
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo usersPromise={fetchUserPromise}/>
+        </Suspense> 
+      </ErrorBoundary>
+
     </main>
   );
 }
